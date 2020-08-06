e39412fb81ffa37ebd3163108b5fe587320b11f6
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-06 22:20:34 +0900

    1-3-6. authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,11 @@
     Hello!
 
     @if (Auth::check())
-      {{ \Auth::user()->name }}さん！
+      {{ \Auth::user()->name }}さん！<br>
+      <a href="/auth/logout">ログアウト</a>
     @else
       ゲストさん！<br>
+      <a href="/auth/login">ログイン</a>
       <a href="/auth/register">会員登録</a>
     @endif
   </body>
